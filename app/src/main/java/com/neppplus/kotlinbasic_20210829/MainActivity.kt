f08c65cf0dfaa54f88e5309603c2238b13895eb7
조경진 <cho881020@gmail.com>
2021-08-29 11:48:24 +0900

    변수 / 조건문 예시

--- app/src/main/java/com/neppplus/kotlinbasic_20210829/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20210829/MainActivity.kt
@@ -11,6 +11,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+//        본인의 이름을 변수 (myName)에 담고, 로그로 출력.
+        var myName = "조경진"  // 자료형? String 세팅.
+        myName = "조진경"
+        Log.d("이름출력-변수활용", myName)
+
+
+
+//        한 사용자의 나이를 17살 이라고 저장.
+        val userAge = 8
+
+//        이 사람이 성인인가?  나이가 20살 이상인가?
+        if (userAge >= 20) {
+//            if ( ) 내부의 질문 결과가 true일때 만 실행됨.
+            Log.d("if문예시", "성인이 맞습니다.")
+        }
+        else if ( userAge >= 17 ) {
+//            20살미만 + 17살 이상인 경우.
+            Log.d("if문예시", "고등학생입니다.")
+        }
+        else if ( userAge >= 14 ) {
+            Log.d("if문예시", "중학생입니다.")
+        }
+        else {
+            Log.d("if문예시", "초등학생 or 미취학아동입니다.")
+        }
+
+
+//        자기소개버튼이 눌리면 => "저는 ???입니다." 토스트로 출력.
+        introduceToastBtn.setOnClickListener {
+
+            Toast.makeText(this, "저는 조경진입니다.", Toast.LENGTH_SHORT).show()
+
+        }
+
         toastBtn.setOnClickListener {
 //            토스트 - "반갑습니다." 문장 띄우기
 
